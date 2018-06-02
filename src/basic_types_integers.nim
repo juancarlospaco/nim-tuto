4ed423230aaabb6734ebe5b220f82829311082ca
Juan Carlos <juan@thirstie.com>
2018-06-02 00:24:58 -0300

    Init

--- src/basic_types_integers.nim
+++ src/basic_types_integers.nim
@@ -9,3 +9,8 @@ var integer_8bits = int8(123)
 var integer_16bits = int16(123)
 var integer_32bits = int32(123)
 var integer_64bits = int64(123)
+
+var normal_integer = 255         # 255
+var binary_integer = 0b11111111  # 255
+var octal_integer = 0o377        # 255
+var hexa_integer = 0xff          # 255
