24a01a16e2c99558ae13e7b4621e00c695d12593
Juan Carlos <juan@thirstie.com>
2018-06-02 00:06:02 -0300

    Init

--- src/basic_types_string.nim
+++ src/basic_types_string.nim
@@ -4,6 +4,12 @@ var whitespaces = " "
 var emptystring = ""
 var fox_strings = "The quick brown fox jumps over the lazy dog"
 
+# Raw string literals and Multiline string literals
+var raw_string_literal = r"Raw string literals"
+var multiline_string = """
+This is a Multiline String, of string type, it can have quote characters inside,
+like ', ", \n , \r, \c, \x00, \t, Emoji, etc without producing any errors. :)
+"""
 
 # Char
 var char_type = 'a'
